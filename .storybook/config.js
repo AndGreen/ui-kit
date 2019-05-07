92557aa74c7a6a1cea8baa5ae90864f2d608e134
zelenin_andrew <zelenin.andrew@mango.rocks>
2019-05-07 22:28:25 +0300

    Added global styles

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,7 @@
-import { configure } from '@storybook/react';
+
+import { addDecorator, configure } from '@storybook/react';
+import * as React from 'react';
+import GlobalStyle from '../src/GlobalStyles';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../src', true, /\.stories\.js$/);
@@ -6,4 +9,12 @@ function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
 
+const withGlobal = (cb) => (
+  <React.Fragment>
+    <GlobalStyle />
+    {cb()}
+  </React.Fragment>
+);
+
+addDecorator(withGlobal);
 configure(loadStories, module);
